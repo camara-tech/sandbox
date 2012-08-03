57aae7438cb5353199febd494f72c9e2403fa647
Jonathan Camara <italy_portugal@yahoo.com>
2012-08-03 14:53:44 -0500

    create window and draw in it.

---
+++ nodeeditor.py
@@ -0,0 +1,14 @@
+import graphics
+import yaml
+
+
+def main():
+    window = graphics.GraphWin('Generic Node Editor', 800, 600, autoflush=False)
+    
+    c=graphics.Circle(graphics.Point(300,400),100)
+    c.draw(window)
+    window.getMouse()
+    window.close()
+    graphics.update()
+    
+main()
