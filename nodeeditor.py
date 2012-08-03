f51e2ccc623c3bf3144d57801bc15379e5b3b557
Jonathan Camara <italy_portugal@yahoo.com>
2012-08-03 00:32:22 -0500

    Started working on node editor
    curious about how a node editor works, so I'm making one.

---
+++ nodeeditor.py
@@ -0,0 +1,17 @@
+#node editor
+#requires PyYAML, stackless python, and graphics.py
+
+import yaml
+import graphics
+
+{"name":"Node","input":"string"}
+
+def main():
+    #start loop
+    #check mouse
+    #check edit boxes
+    #update information
+    #update screen
+    #end loop
+
+    
