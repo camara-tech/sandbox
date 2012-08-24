e2f295143ffbd09556fe2bf8ff6b85032f60138a
Jonathan Camara <italy_portugal@yahoo.com>
2012-08-23 21:17:03 -0500

    added roadmap in comments and main() docstring

--- nodeeditor.py
+++ nodeeditor.py
@@ -1,5 +1,7 @@
 import pygame
 def main():
+"""Initializes, runs the main/master loop"""
+
     #Initialize Pygame
     pygame.init()
 
@@ -15,3 +17,22 @@ def main():
         if pygame.event.peek(pygame.KEYDOWN):
             quit=True
 main()
+
+#step one, show window
+#step two, draw box in window
+#step three, draw 2 boxes in window
+#step four, draw input and output sections on boxes
+#step five, draw line between input and output sections
+#step six, be able to move boxes around while keeping line attached.
+#step seven, add buttons to boxes
+#step eight, be able to add boxes
+#step nine, be able to remove boxes
+#step ten, be able to connect all the boxes
+#step eleven, be able to store layout and connections of boxes
+#step twelve, add "meanings" to boxes
+#step thirteen, parse layout of boxes and execute meanings
+#step fourteen, add textboxes
+#step fifteen, add color pickers
+#step sixteen, add visualization tools
+#step seventeen, add language parsers
+
