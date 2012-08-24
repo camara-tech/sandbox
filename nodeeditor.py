f434cb214c2cec79ab5086809f724ceea72da348
Jonathan Camara <italy_portugal@yahoo.com>
2012-08-23 20:27:25 -0500

    added pomodoro, update readme, nodeeditor

--- nodeeditor.py
+++ nodeeditor.py
@@ -1,16 +1,11 @@
-# node editor
-# requires PyYAML, stackless python, and graphics.py
-import graphics
+import pygame
 import yaml
-
+import sys
+node = ["name":"Test Node", "input":[floating,integer,string],
+        "function":"add","output":string]
+def drawNode():
 
 def main():
-    window = graphics.GraphWin('Generic Node Editor', 800, 600, autoflush=False)
-    
-    c=graphics.Circle(graphics.Point(300,400),100)
-    c.draw(window)
-    window.getMouse()
-    window.close()
-    graphics.update()
+    pygame.init()
     
 main()
---
+++ pomodoro.py
@@ -0,0 +1,20 @@
+import time
+
+def pomodoro_timer():
+    work_time=25*60
+    break_time=5*60
+    
+def pomodoro_todo():
+    place = ["Home","Veritas","Magdalen"]
+    date = time.ctime()
+    author = "Jonathan Camara"
+    todo_list = [["task1","priority"],["task2","priority"]]
+    unexpected = [["task","unplanned"],["task","emergency"]]
+    
+    
+def pomodoro_activity():
+    activity = []
+
+def pomodoro_records():
+    records = []
+
