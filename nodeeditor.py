e7a56b513095b2fd06b02b0ca88c1268cc7b6d98
Jonathan Camara <italy_portugal@yahoo.com>
2013-05-12 08:31:57 -0500

    initial debug system and cursor tracking

--- nodeeditor.py
+++ nodeeditor.py
@@ -1,15 +1,17 @@
+#Iteration 1: load a window and set it's caption
+#Iteration 2: escape key quits, Track the Mouse and display debug info
 import yaml
 import pygame
 
 def main():
 # Load window
-    #Set Window Size
+    #Set Window Size and graphical context
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    screen = pygame.display.set_mode((800,600),pygame.RESIZABLE,0)
     #set window title
     pygame.display.set_caption("Node Editor")
-    #set graphical context
     #load media
+    
     #draw buttons
     #draw canvas
     
@@ -19,7 +21,20 @@ def main():
     #test if opening medium or new medium
 #main loop
     while quit == False:
-        quit==True
+        #set escape sequence
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                quit = True
+        
+        #Track the Mouse
+        cursor=pygame.mouse.get_pos()
+        #display debug info
+        debug_font = pygame.font.Font(None,20)
+        debug_cursorposition = debug_font.render(str(cursor),True,(200,200,200))
+        debug_pos = (20,20)
+        screen.blit(debug_cursorposition,debug_pos)
+        pygame.display.flip()
+        screen.fill((0,0,0))
 #Manipulate Nodes
     #add nodes
     #remove nodes and connectors
@@ -27,5 +42,6 @@ def main():
     #move nodes and connectors
     #focus on node
     #update node info
-    
+
+main()
     
